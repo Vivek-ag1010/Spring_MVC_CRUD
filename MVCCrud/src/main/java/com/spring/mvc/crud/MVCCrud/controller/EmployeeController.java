90fc26e4ce70390bd1838db3098f75bee1159366
Vivek <vivek.agrahari@xcelore.com>
2024-02-19 15:35:13 +0530

    Update Employee

--- MVCCrud/src/main/java/com/spring/mvc/crud/MVCCrud/controller/EmployeeController.java
+++ MVCCrud/src/main/java/com/spring/mvc/crud/MVCCrud/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.spring.mvc.crud.MVCCrud.entity.Employee;
 import com.spring.mvc.crud.MVCCrud.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -52,5 +49,17 @@ public class EmployeeController {
         employeeService.save(theEmployee);
         //use a redirect to prevent duplicate submissions
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdated(@RequestParam("employeeId") int theID, Model theModel)
+    {
+        //get the employee from the service
+        Employee theEmployee=employeeService.findById(theID);
+        //set employee in the model to prepopulate the form
+        theModel.addAttribute("employee", theEmployee);
+        //send over to our form
+
+        return "employees/employee-form";
     }
 }
